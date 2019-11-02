075c704fc5d091434ee6e10f5d2cfd5e603311fb
inbraev <ibraev.nazar@yandex.ru>
2019-11-02 14:40:41 +0600

    rest

--- rest/restful/course/serializers.py
+++ rest/restful/course/serializers.py
@@ -1,26 +1,41 @@
 from rest_framework import serializers
 from .models import *
 
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
 class ContactsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contacts
-        fields = '__all__'
+        fields = ['type','value']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields =['name','imgpath']
 
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
-        fields = '__all__'
+        fields = ['longitude',"latitude","address"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    contacts = ContactsSerializer(many=True)
+    branches = BranchSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'category', 'logo', 'branches', 'contacts']
+
+    def create(self, validated_data):
+        branches_data = validated_data.pop('branches')
+        contacts_data = validated_data.pop('contacts')
+        course = Course.objects.create(**validated_data)
+
+        for branch_data in branches_data:
+            Branch.objects.create(course=course, **branch_data)
+
+        for contact_data in contacts_data:
+            Contacts.objects.create(course=course, **contact_data)
+
+        return course
